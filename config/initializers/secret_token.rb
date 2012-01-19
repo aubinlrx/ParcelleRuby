9323dc0f8dba34d7ca502f1e8d6a4aaced81ee82
aubindesign <aubin.lorieux@supinfo.com>
2012-01-19 16:16:24 +0000

    Version de base
    Réecriture de l'application de gestion parcellaire en Ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParcelleRuby::Application.config.secret_token = '9a38fd335b46114af25b2d32c3005d5af6dbd613f348f79be45e0747c430664eceab462cf4bcced343d5b3836aec04d4951a7c46f4b74e11c7e0513709be5e19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ParcelleRuby::Application.config.session_store :cookie_store, key: '_ParcelleRuby_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ParcelleRuby::Application.config.session_store :active_record_store
